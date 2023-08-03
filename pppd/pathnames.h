martinetd/ppp

commit 66a8c74c3f73d7480a09923a225b56b8829ae790

Let ./configure control the paths for pppd
This was previously done by specifying an overriding value for _ROOT_PATH. With this change, this variable is now gone.
Instead, pathnames.h will use the SYSCONFDIR and LOCALSTATEDIR to resolve these paths. These directories is already controlled by
configure.

Package maintainers should be aware though that this may change their current configuration. The convential ./configure way is to
specify:
   ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var/run --with-plugin-dir=/usr/lib/pppd/2.4.10

If one omit the --sysconfdir option, then the default location is by ${prefix}/etc which may not be what you want.

Signed-off-by: Eivind Næss <eivnaes@yahoo.com>

configure.ac
@@ -194,7 +194,7 @@ AS_IF([test "x$enable_openssl_engine" != "xno"], [],
 #
 # Specify runtime directory
 AC_ARG_WITH([plugin-dir],
-	AC_HELP_STRING([--with-plugin-dir=DIR], [Specify the plugin directory for pppd]))
+	AS_HELP_STRING([--with-plugin-dir=DIR],[Specify the plugin directory for pppd]))
 AS_IF([test -n "$with_plugin_dir"],
 	[PPPD_PLUGIN_DIR="$with_plugin_dir"],
 	[PPPD_PLUGIN_DIR="${libdir}/pppd/$VERSION"])
@@ -203,19 +203,19 @@ AC_SUBST(PPPD_PLUGIN_DIR, "$PPPD_PLUGIN_DIR", [The pppd plugin directory])
 #
 # Specify runtime directory
 AC_ARG_WITH([runtime-dir],
-	AC_HELP_STRING([--with-runtime-dir=DIR], [Specify the runtime directory for pppd]))
+	AS_HELP_STRING([--with-runtime-dir=DIR],[Specify the runtime directory for pppd]))
 AS_IF([test -n "$with_runtime_dir"],
 	[PPPD_RUNTIME_DIR="$with_runtime_dir"],
-	[PPPD_RUNTIME_DIR="${localstatedir}/run/pppd"])
+	[PPPD_RUNTIME_DIR="${runstatedir}/pppd"])
 AC_SUBST(PPPD_RUNTIME_DIR)
 
 #
 # Specify runtime directory
 AC_ARG_WITH([logfile-dir],
-	AC_HELP_STRING([--with-logfile-dir=DIR], [Specify the log directory for pppd]))
+	AS_HELP_STRING([--with-logfile-dir=DIR],[Specify the log directory for pppd]))
 AS_IF([test -n "$with_logfile_dir"],
 	[PPPD_LOGFILE_DIR="$with_logfile_dir"],
-	[PPPD_LOGFILE_DIR="${localstatedir}/log/pppd"])
+	[PPPD_LOGFILE_DIR="${localstatedir}/log/ppp"])
 AC_SUBST(PPPD_LOGFILE_DIR)
 
 #

pppd/Makefile.am
@@ -68,7 +68,7 @@ pppd_SOURCES = \
     upap.c \
     utils.c
 
-pppd_CPPFLAGS = -DPPPD_RUNTIME_DIR='"@PPPD_RUNTIME_DIR@"' -DPPPD_LOGFILE_DIR='"@PPPD_LOGFILE_DIR@"'
+pppd_CPPFLAGS = -DSYSCONFDIR=\"${sysconfdir}\" -DLOCALSTATEDIR=\"${localstatedir}\" -DPPPD_RUNTIME_DIR='"@PPPD_RUNTIME_DIR@"' -DPPPD_LOGFILE_DIR='"@PPPD_LOGFILE_DIR@"'
 pppd_LDFLAGS =
 pppd_LIBS =
 

pppd/pathnames.h
@@ -1,5 +1,46 @@
 /*
- * define path names
+ * pathnames.h - define default paths for pppd
+ *
+ * Copyright (c) 1999-2002 Paul Mackerras. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. The name(s) of the authors of this software must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Paul Mackerras
+ *     <paulus@samba.org>".
+ *
+ * THE AUTHORS OF THIS SOFTWARE DISCLAIM ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * Paths are controlled by configure. Typically, one would pass the following options
+ * to configure:
+ *    ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var
+ *
+ * Note that if the prefix variable do control all the other directory variables, e.g
+ * sysconfdir is ${prefix}/etc. Setting prefix to /usr, you'll have to override
+ * sysconfdir with /etc to avoid installing config files into /usr/etc.
+ *
+ * In addition, there are three explicit variables that has overrides via configure:
+ * - PPPD_RUNTIME_DIR, set by --with-runtime-dir=<dir>
+ * - PPPD_LOGFILE_DIR, set by --with-logfile-dir=<dir>
+ * - PPPD_PLUGIN_DIR, set by --with-plugin-dir=<dir>
  */
 #ifndef PPP_PATHNAMES_H
 #define PPP_PATHNAMES_H
@@ -11,66 +52,82 @@
 #endif /* HAVE_PATHS_H */
 
 #ifndef _PATH_DEVNULL
-#define _PATH_DEVNULL	"/dev/null"
+#define _PATH_DEVNULL           "/dev/null"
 #endif
 
+#define PPP_DEVNULL             _PATH_DEVNULL
+
+/*
+ * PPPD_RUNTIME_DIR is set by passing --with-runtime-dir=<path> via configure
+ *    the default value set by configure when option is absent is ${localstatedir}/run/pppd
+ */
 #ifdef PPPD_RUNTIME_DIR
-#undef  _PATH_VARRUN
-#define _PATH_VARRUN 	PPPD_RUNTIME_DIR "/"
-#endif
-
-#ifdef PPPD_LOGFILE_DIR
-#undef  _PATH_VARLOG
-#define _PATH_VARLOG    PPPD_LOGFILE_DIR
-#endif
-
-#ifdef PPPD_PLUGIN_DIR
-#define _PATH_PLUGIN    PPPD_PLUGIN_DIR
+#define PPP_PATH_VARRUN         PPPD_RUNTIME_DIR
 #else
-#ifdef __STDC__
-#define _PATH_PLUGIN	DESTDIR "/lib/pppd/" VERSION
-#else  /* __STDC__ */
-#define _PATH_PLUGIN	"/usr/lib/pppd"
-#endif /* __STDC__ */
+#define PPP_PATH_VARRUN         _PATH_VARRUN
+#endif
+
+/*
+ * PPPD_LOGFILE_DIR is set by passing --with-logfile-dir=<path> via configure
+ *    the default value set by configure when option is absent is ${localstatedir}/log/ppp
+ */
+#ifdef PPPD_LOGFILE_DIR
+#define PPP_PATH_VARLOG         PPPD_LOGFILE_DIR
+#else
+#define PPP_PATH_VARLOG         _PATH_VARRUN "/log/ppp"
+#endif
+
+/*
+ * PPPD_PLUGIN_DIR is set by passing --with-plugin-dir=<path> via configure
+ *    the default value set by configure when option is absent is ${libdir}/pppd/${version}
+ */
+#ifdef PPPD_PLUGIN_DIR
+#define PPP_PATH_PLUGIN         PPPD_PLUGIN_DIR
 #endif /* PPPD_PLUGIN_DIR */
 
-#ifndef _ROOT_PATH
-#define _ROOT_PATH
-#endif
+#define PPP_PATH_CONFDIR        SYSCONFDIR "/ppp"
 
-#define _PATH_UPAPFILE 	 _ROOT_PATH "/etc/ppp/pap-secrets"
-#define _PATH_CHAPFILE 	 _ROOT_PATH "/etc/ppp/chap-secrets"
-#define _PATH_SRPFILE 	 _ROOT_PATH "/etc/ppp/srp-secrets"
+#define PPP_PATH_UPAPFILE       PPP_PATH_CONFDIR "/pap-secrets"
+#define PPP_PATH_CHAPFILE       PPP_PATH_CONFDIR "/chap-secrets"
+#define PPP_PATH_SRPFILE        PPP_PATH_CONFDIR "/srp-secrets"
 
 #ifdef PPP_WITH_EAPTLS
-#define _PATH_EAPTLSCLIFILE	_ROOT_PATH "/etc/ppp/eaptls-client"
-#define _PATH_EAPTLSSERVFILE	_ROOT_PATH "/etc/ppp/eaptls-server"
-#define _PATH_OPENSSLCONFFILE	_ROOT_PATH "/etc/ppp/openssl.cnf"
+#define PPP_PATH_EAPTLSCLIFILE  PPP_PATH_CONFDIR "/eaptls-client"
+#define PPP_PATH_EAPTLSSERVFILE PPP_PATH_CONFDIR "/eaptls-server"
+#define PPP_PATH_OPENSSLCONFFILE PPP_PATH_CONFDIR "/openssl.cnf"
 #endif /* PPP_WITH_EAPTLS */
 
-#define _PATH_SYSOPTIONS _ROOT_PATH "/etc/ppp/options"
-#define _PATH_IPUP	 _ROOT_PATH "/etc/ppp/ip-up"
-#define _PATH_IPDOWN	 _ROOT_PATH "/etc/ppp/ip-down"
-#define _PATH_IPPREUP	 _ROOT_PATH "/etc/ppp/ip-pre-up"
-#define _PATH_AUTHUP	 _ROOT_PATH "/etc/ppp/auth-up"
-#define _PATH_AUTHDOWN	 _ROOT_PATH "/etc/ppp/auth-down"
-#define _PATH_TTYOPT	 _ROOT_PATH "/etc/ppp/options."
-#define _PATH_CONNERRS	 _ROOT_PATH _PATH_VARLOG "/connect-errors"
-#define _PATH_PEERFILES	 _ROOT_PATH "/etc/ppp/peers/"
-#define _PATH_RESOLV	 _ROOT_PATH "/etc/ppp/resolv.conf"
+#define PPP_PATH_SYSOPTIONS     PPP_PATH_CONFDIR "/options"
+#define PPP_PATH_IPUP           PPP_PATH_CONFDIR "/ip-up"
+#define PPP_PATH_IPDOWN         PPP_PATH_CONFDIR "/ip-down"
+#define PPP_PATH_IPPREUP        PPP_PATH_CONFDIR "/ip-pre-up"
+#define PPP_PATH_AUTHUP         PPP_PATH_CONFDIR "/auth-up"
+#define PPP_PATH_AUTHDOWN       PPP_PATH_CONFDIR "/auth-down"
+#define PPP_PATH_TTYOPT         PPP_PATH_CONFDIR "/options."
+#define PPP_PATH_PEERFILES      PPP_PATH_CONFDIR "/peers/"
+#define PPP_PATH_RESOLV         PPP_PATH_CONFDIR "/resolv.conf"
 
-#define _PATH_USEROPT	 ".ppprc"
-#define	_PATH_PSEUDONYM	 ".ppp_pseudonym"
+#define PPP_PATH_CONNERRS       PPP_PATH_VARLOG  "/connect-errors"
+
+#define PPP_PATH_USEROPT        ".ppprc"
+#define PPP_PATH_PSEUDONYM	    ".ppp_pseudonym"
 
 #ifdef PPP_WITH_IPV6CP
-#define _PATH_IPV6UP     _ROOT_PATH "/etc/ppp/ipv6-up"
-#define _PATH_IPV6DOWN   _ROOT_PATH "/etc/ppp/ipv6-down"
+#define PPP_PATH_IPV6UP         PPP_PATH_CONFDIR "/ipv6-up"
+#define PPP_PATH_IPV6DOWN       PPP_PATH_CONFDIR "/ipv6-down"
 #endif
 
-#ifdef __STDC__
-#define _PATH_PPPDB	_ROOT_PATH _PATH_VARRUN "pppd2.tdb"
-#else /* __STDC__ */
-#define _PATH_PPPDB	_PPP_VARRUN "pppd2.tdb"
-#endif /* __STDC__ */
+#define PPP_PATH_PPPDB          PPP_PATH_VARRUN  "/pppd2.tdb"
+
+#ifdef __linux__
+#define PPP_PATH_LOCKDIR        PPP_PATH_VARRUN  "/lock"
+#else
+#ifdef SVR4
+#define PPP_PATH_LOCKDIR        LOCALSTATEDIR "/spool/locks"
+#else
+#define PPP_PATH_LOCKDIR        LOCALSTATEDIR "/spool/lock"
+#endif
+#endif
+
 
 #endif /* PPP_PATHNAMES_H */

pppd/plugins/pppoe/Makefile.am
@@ -6,7 +6,7 @@ dist_man8_MANS = pppoe-discovery.8
 noinst_HEADERS = \
     pppoe.h
 
-pppoe_la_CPPFLAGS = -I${top_srcdir}
+pppoe_la_CPPFLAGS = -I${top_srcdir} -DSYSCONFDIR=\"${sysconfdir}\"
 pppoe_la_LDFLAGS = -module -avoid-version
 pppoe_la_SOURCES = plugin.c discovery.c if.c common.c
 

pppd/plugins/radius/Makefile.am
@@ -47,7 +47,7 @@ radrealms_la_SOURCES = radrealms.c
 libradiusclient_la_SOURCES = \
     avpair.c buildreq.c config.c dict.c ip_util.c \
 	clientid.c sendserver.c lock.c util.c
-libradiusclient_la_CPPFLAGS = $(RADIUS_CPPFLAGS)
+libradiusclient_la_CPPFLAGS = $(RADIUS_CPPFLAGS) -DSYSCONFDIR=\"${sysconfdir}\"
 
 if !WITH_OPENSSL
 libradiusclient_la_SOURCES += md5.c

pppd/plugins/radius/pathnames.h
@@ -17,12 +17,12 @@
 #ifndef PATHNAMES_H
 #define PATHNAMES_H
 
-#define _PATH_DEV_URANDOM	"/dev/urandom"		/* Linux only */
-#define _PATH_ETC_ISSUE		"/etc/issue"
+#define PPP_PATH_DEV_URANDOM	"/dev/urandom"		/* Linux only */
+#define PPP_PATH_ETC_ISSUE		"/etc/issue"
 
 /* normally defined in the Makefile */
-#ifndef _PATH_ETC_RADIUSCLIENT_CONF
-#define _PATH_ETC_RADIUSCLIENT_CONF       "/etc/radiusclient.conf"
+#ifndef PPP_PATH_ETC_RADIUSCLIENT_CONF
+#define PPP_PATH_ETC_RADIUSCLIENT_CONF       SYSCONFDIR "/radiusclient.conf"
 #endif
 
 #endif /* PATHNAMES_H */

pppd/auth.c
@@ -750,7 +750,7 @@ link_down(int unit)
 	if (auth_script_state == s_up && auth_script_pid == 0) {
 	    update_link_stats(unit);
 	    auth_script_state = s_down;
-	    auth_script(_PATH_AUTHDOWN);
+	    auth_script(PPP_PATH_AUTHDOWN);
 	}
     }
     if (!doing_multilink) {
@@ -899,7 +899,7 @@ network_phase(int unit)
 	auth_state = s_up;
 	if (auth_script_state == s_down && auth_script_pid == 0) {
 	    auth_script_state = s_up;
-	    auth_script(_PATH_AUTHUP);
+	    auth_script(PPP_PATH_AUTHUP);
 	}
     }
 
@@ -1489,7 +1489,7 @@ check_passwd(int unit,
      * Open the file of pap secrets and scan for a suitable secret
      * for authenticating this user.
      */
-    filename = _PATH_UPAPFILE;
+    filename = PPP_PATH_UPAPFILE;
     addrs = opts = NULL;
     ret = UPAP_AUTHNAK;
     f = fopen(filename, "r");
@@ -1590,7 +1590,7 @@ null_login(int unit)
      * Open the file of pap secrets and scan for a suitable secret.
      */
     if (ret <= 0) {
-	filename = _PATH_UPAPFILE;
+	filename = PPP_PATH_UPAPFILE;
 	addrs = NULL;
 	f = fopen(filename, "r");
 	if (f == NULL)
@@ -1637,7 +1637,7 @@ get_pap_passwd(char *passwd)
 	    return ret;
     }
 
-    filename = _PATH_UPAPFILE;
+    filename = PPP_PATH_UPAPFILE;
     f = fopen(filename, "r");
     if (f == NULL)
 	return 0;
@@ -1674,7 +1674,7 @@ have_pap_secret(int *lacks_ipp)
 	    return ret;
     }
 
-    filename = _PATH_UPAPFILE;
+    filename = PPP_PATH_UPAPFILE;
     f = fopen(filename, "r");
     if (f == NULL)
 	return 0;
@@ -1716,7 +1716,7 @@ have_chap_secret(char *client, char *server,
 	}
     }
 
-    filename = _PATH_CHAPFILE;
+    filename = PPP_PATH_CHAPFILE;
     f = fopen(filename, "r");
     if (f == NULL)
 	return 0;
@@ -1754,7 +1754,7 @@ have_srp_secret(char *client, char *server, int need_ip, int *lacks_ipp)
     char *filename;
     struct wordlist *addrs;
 
-    filename = _PATH_SRPFILE;
+    filename = PPP_PATH_SRPFILE;
     f = fopen(filename, "r");
     if (f == NULL)
 	return 0;
@@ -1802,7 +1802,7 @@ get_secret(int unit, char *client, char *server,
 	    return 0;
 	}
     } else {
-	filename = _PATH_CHAPFILE;
+	filename = PPP_PATH_CHAPFILE;
 	addrs = NULL;
 	secbuf[0] = 0;
 
@@ -1856,7 +1856,7 @@ get_srp_secret(int unit, char *client, char *server,
     if (!am_server && passwd[0] != '\0') {
 	strlcpy(secret, passwd, MAXWORDLEN);
     } else {
-	filename = _PATH_SRPFILE;
+	filename = PPP_PATH_SRPFILE;
 	addrs = NULL;
 
 	fp = fopen(filename, "r");
@@ -2359,13 +2359,13 @@ auth_script_done(void *arg)
     case s_up:
 	if (auth_state == s_down) {
 	    auth_script_state = s_down;
-	    auth_script(_PATH_AUTHDOWN);
+	    auth_script(PPP_PATH_AUTHDOWN);
 	}
 	break;
     case s_down:
 	if (auth_state == s_up) {
 	    auth_script_state = s_up;
-	    auth_script(_PATH_AUTHUP);
+	    auth_script(PPP_PATH_AUTHUP);
 	}
 	break;
     }
@@ -2419,7 +2419,7 @@ have_eaptls_secret_server(char *client, char *server,
     char cacertfile[MAXWORDLEN];
     char pkfile[MAXWORDLEN];
 
-    filename = _PATH_EAPTLSSERVFILE;
+    filename = PPP_PATH_EAPTLSSERVFILE;
     f = fopen(filename, "r");
     if (f == NULL)
 		return 0;
@@ -2476,7 +2476,7 @@ have_eaptls_secret_client(char *client, char *server)
 	if (pkcs12_file)
 		return 1;
 
-    filename = _PATH_EAPTLSCLIFILE;
+    filename = PPP_PATH_EAPTLSCLIFILE;
     f = fopen(filename, "r");
     if (f == NULL)
 		return 0;
@@ -2694,7 +2694,7 @@ get_eaptls_secret(int unit, char *client, char *server,
 	}
 	else
 	{
-		filename = (am_server ? _PATH_EAPTLSSERVFILE : _PATH_EAPTLSCLIFILE);
+		filename = (am_server ? PPP_PATH_EAPTLSSERVFILE : PPP_PATH_EAPTLSCLIFILE);
 		addrs = NULL;
 
 		fp = fopen(filename, "r");

pppd/eap-tls.c
@@ -158,10 +158,10 @@ CONF *eaptls_ssl_load_config( void )
 
     config = NCONF_new( NULL );
     dbglog( "Loading OpenSSL config file" );
-    ret_code = NCONF_load( config, _PATH_OPENSSLCONFFILE, &error_line );
+    ret_code = NCONF_load( config, PPP_PATH_OPENSSLCONFFILE, &error_line );
     if (ret_code == 0)
     {
-        warn( "EAP-TLS: Error in OpenSSL config file %s at line %d", _PATH_OPENSSLCONFFILE, error_line );
+        warn( "EAP-TLS: Error in OpenSSL config file %s at line %d", PPP_PATH_OPENSSLCONFFILE, error_line );
         NCONF_free( config );
         config = NULL;
         ERR_clear_error();

commit 8c5cc17bf0da5719f10ce0fccad88c8dd2a681f5

Make lock dir configurable
lock dir changed on linux from /var/log to /run/pppd/lock with
pppd-2.5.0, which makes pppd fail to start if the distribution does not
pre-create the directory.

In the line of configurability, also make the lock dir configurable
through ./configure --with-lock-dir for distributions that prefer
/run/pppd/lock, but change the default for linux back to /var/lock

This commit is bigger than it should be, because the current configure
mechanism for PPPD_RUNTIME_DIR and others is inconditional: pathnames.h
has #else cases for when the variable is not defined but that is not
possible.
This does not work for lock dir, because on non-linux platform the path
is different, and reproducing this logic at configure time is not
trivial.

Instead of the current method, only set the the PPPD_X_DIR variables in
pppdconf.h if it was explicitly set in configure, and otherwise leave it
to the else case.

Note:
 - the else cases were incorrect for runtime dir and log dirs, and have
   been fixed
 - PPPD_PLUGIN_DIR has been kept as is because it is used for rpath in
   makefiles and needs to be set inconditionnaly at configure time

Fixes: 66a8c74c3f73 ("Let ./configure control the paths for pppd")
Fixes: #419
Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

configure.ac
@@ -209,18 +209,24 @@ AC_SUBST(PPPD_PLUGIN_DIR, "$PPPD_PLUGIN_DIR", [The pppd plugin directory])
 AC_ARG_WITH([runtime-dir],
 	AS_HELP_STRING([--with-runtime-dir=DIR],[Specify the runtime directory for pppd]))
 AS_IF([test -n "$with_runtime_dir"],
-	[PPPD_RUNTIME_DIR="$with_runtime_dir"],
-	[PPPD_RUNTIME_DIR="${runstatedir}/pppd"])
-AC_SUBST(PPPD_RUNTIME_DIR)
+	[PPPD_RUNTIME_DIR="$with_runtime_dir";
+	 AC_DEFINE_UNQUOTED(PPPD_RUNTIME_DIR, "$PPPD_RUNTIME_DIR", [The pppd runtime directory])])
 
 #
-# Specify runtime directory
+# Specify lock directory
+AC_ARG_WITH([lock-dir],
+	AS_HELP_STRING([--with-lock-dir=DIR],[Specify the lock directory for pppd]))
+AS_IF([test -n "$with_lock_dir"],
+	[PPPD_LOCK_DIR="$with_lock_dir";
+	 AC_DEFINE_UNQUOTED(PPPD_LOCK_DIR, "$PPPD_LOCK_DIR", [The pppd lock directory])])
+
+#
+# Specify logfile directory
 AC_ARG_WITH([logfile-dir],
 	AS_HELP_STRING([--with-logfile-dir=DIR],[Specify the log directory for pppd]))
 AS_IF([test -n "$with_logfile_dir"],
-	[PPPD_LOGFILE_DIR="$with_logfile_dir"],
-	[PPPD_LOGFILE_DIR="${localstatedir}/log/ppp"])
-AC_SUBST(PPPD_LOGFILE_DIR)
+	[PPPD_LOGFILE_DIR="$with_logfile_dir"];
+	 AC_DEFINE_UNQUOTED(PPPD_LOGFILE_DIR, "$PPPD_LOGFILE_DIR", [The pppd logfile directory])])
 
 #
 # System CA certificates path
@@ -410,8 +416,9 @@ Setting up SunOS kernel module(s)"
 echo "
 $PACKAGE_NAME version $PACKAGE_VERSION
     Prefix...............: $prefix
-    Runtime Dir..........: $PPPD_RUNTIME_DIR
-    Logfile Dir..........: $PPPD_LOGFILE_DIR
+    Runtime Dir..........: ${PPPD_RUNTIME_DIR:-system default}
+    Lock Dir.............: ${PPPD_LOCK_DIR:-system default}
+    Logfile Dir..........: ${PPPD_LOGFILE_DIR:-system default}
     Plugin Dir...........: $PPPD_PLUGIN_DIR
     System CA Path ......: ${SYSTEM_CA_PATH:-not set}
     With OpenSSL.........: ${with_openssl:-yes}

pppd/Makefile.am
@@ -3,11 +3,11 @@ dist_man8_MANS = pppd.8
 check_PROGRAMS =
 
 utest_chap_SOURCES = chap_ms.c utils.c crypto_ms.c
-utest_chap_CPPFLAGS = -DUNIT_TEST
+utest_chap_CPPFLAGS = -DUNIT_TEST -DLOCALSTATEDIR=\"${localstatedir}\"
 utest_chap_LDFLAGS =
 
 utest_peap_SOURCES = peap.c utils.c mppe.c
-utest_peap_CPPFLAGS = -DUNIT_TEST
+utest_peap_CPPFLAGS = -DUNIT_TEST -DLOCALSTATEDIR=\"${localstatedir}\"
 utest_peap_LDFLAGS =
 
 utest_crypto_SOURCES = crypto.c
@@ -83,7 +83,7 @@ pppd_SOURCES = \
     upap.c \
     utils.c
 
-pppd_CPPFLAGS = -DSYSCONFDIR=\"${sysconfdir}\" -DLOCALSTATEDIR=\"${localstatedir}\" -DPPPD_RUNTIME_DIR='"@PPPD_RUNTIME_DIR@"' -DPPPD_LOGFILE_DIR='"@PPPD_LOGFILE_DIR@"'
+pppd_CPPFLAGS = -DSYSCONFDIR=\"${sysconfdir}\" -DLOCALSTATEDIR=\"${localstatedir}\"
 pppd_LDFLAGS =
 pppd_LIBS =
 

pppd/pathnames.h
@@ -37,10 +37,14 @@
  * sysconfdir is ${prefix}/etc. Setting prefix to /usr, you'll have to override
  * sysconfdir with /etc to avoid installing config files into /usr/etc.
  *
- * In addition, there are three explicit variables that has overrides via configure:
- * - PPPD_RUNTIME_DIR, set by --with-runtime-dir=<dir>
- * - PPPD_LOGFILE_DIR, set by --with-logfile-dir=<dir>
- * - PPPD_PLUGIN_DIR, set by --with-plugin-dir=<dir>
+ * In addition, there are explicit variables that have overrides via configure:
+ * - SYSCONFDIR, set by --sysconfdir=<dir> or its platform default
+ * - LOCALSATEDIR, set by --localstatedir=<dir> or its platform default
+ * - PPPD_RUNTIME_DIR, set by --with-runtime-dir=<dir> or undef
+ * - PPPD_LOCK_DIR, set by --with-lock-dir=<dir> or undef
+ * - PPPD_LOGFILE_DIR, set by --with-logfile-dir=<dir> or undef
+ * If pppd is built with plugins, additionally:
+ * - PPPD_PLUGIN_DIR, set by --with-plugin-dir=<dir> or its platform default
  */
 #ifndef PPP_PATHNAMES_H
 #define PPP_PATHNAMES_H
@@ -59,22 +63,39 @@
 
 /*
  * PPPD_RUNTIME_DIR is set by passing --with-runtime-dir=<path> via configure
- *    the default value set by configure when option is absent is ${localstatedir}/run/pppd
  */
 #ifdef PPPD_RUNTIME_DIR
 #define PPP_PATH_VARRUN         PPPD_RUNTIME_DIR
 #else
-#define PPP_PATH_VARRUN         _PATH_VARRUN
+#define PPP_PATH_VARRUN         _PATH_VARRUN "/pppd"
+#endif
+
+/*
+ * PPPD_LOCK_DIR is set by passing --with-lock-dir=<path> via configure
+ */
+#ifdef PPPD_LOCK_DIR
+#define PPP_PATH_LOCKDIR         PPPD_LOCK_DIR
+#else
+#ifdef __linux__
+#define PPP_PATH_LOCKDIR        LOCALSTATEDIR "/lock"
+#else
+#ifdef SVR4
+#define PPP_PATH_LOCKDIR        LOCALSTATEDIR "/spool/locks"
+#else
+#define PPP_PATH_LOCKDIR        LOCALSTATEDIR "/spool/lock"
+#endif
+#endif
+
+
 #endif
 
 /*
  * PPPD_LOGFILE_DIR is set by passing --with-logfile-dir=<path> via configure
- *    the default value set by configure when option is absent is ${localstatedir}/log/ppp
  */
 #ifdef PPPD_LOGFILE_DIR
 #define PPP_PATH_VARLOG         PPPD_LOGFILE_DIR
 #else
-#define PPP_PATH_VARLOG         _PATH_VARRUN "/log/ppp"
+#define PPP_PATH_VARLOG         LOCALSTATEDIR "log/ppp"
 #endif
 
 /*
@@ -118,16 +139,5 @@
 #endif
 
 #define PPP_PATH_PPPDB          PPP_PATH_VARRUN  "/pppd2.tdb"
-
-#ifdef __linux__
-#define PPP_PATH_LOCKDIR        PPP_PATH_VARRUN  "/lock"
-#else
-#ifdef SVR4
-#define PPP_PATH_LOCKDIR        LOCALSTATEDIR "/spool/locks"
-#else
-#define PPP_PATH_LOCKDIR        LOCALSTATEDIR "/spool/lock"
-#endif
-#endif
-
 
 #endif /* PPP_PATHNAMES_H */

pppd/pppdconf.h.in
@@ -76,4 +76,13 @@
 /* The pppd version */
 #undef PPPD_VERSION
 
+/* runtime dir as per configure's --with-runtime-dir */
+#undef PPPD_RUNTIME_DIR
+
+/* lock dir as per configure's --with-lock-dir */
+#undef PPPD_LOCK_DIR
+
+/* logfile dir as per configure's --with-logfile-dir */
+#undef PPPD_LOGFILE_DIR
+
 #endif